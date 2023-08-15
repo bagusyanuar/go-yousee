494987b5c9e883ad96b640abbba5457b0e3812c2
bagusyanuar <50134851+bagusyanuar@users.noreply.github.com>
2023-08-15 15:48:03 +0700

    city

--- app/admin/controller/city.go
+++ app/admin/controller/city.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/bagusyanuar/go-yousee/app/admin/request"
@@ -8,6 +9,7 @@ import (
 	"github.com/bagusyanuar/go-yousee/common"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
 type City struct {
@@ -90,8 +92,93 @@ func (c *City) GetData(ctx *fiber.Ctx) error {
 	})
 }
 
+func (c *City) Patch(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+	var request request.CityRequest
+
+	if err := ctx.BodyParser(&request); err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(common.APIResponse{
+			Code:    fiber.StatusInternalServerError,
+			Message: fmt.Sprintf("internal server error : %s", err.Error()),
+		})
+	}
+
+	// validate form request
+	validate := validator.New()
+	v := &common.CustomValidator{
+		Validator: validate,
+	}
+
+	if errs := v.Validate(&request); len(errs) > 0 {
+		return ctx.Status(fiber.StatusBadRequest).JSON(common.APIResponse{
+			Code:    fiber.StatusBadRequest,
+			Message: "invalid request",
+			Data:    errs,
+		})
+	}
+
+	data, err := c.cityService.Patch(id, request)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(common.APIResponse{
+			Code:    fiber.StatusInternalServerError,
+			Message: fmt.Sprintf("internal server error : %s", err.Error()),
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(common.APIResponse{
+		Code:    fiber.StatusCreated,
+		Message: "success",
+		Data:    data,
+	})
+}
+
+func (c *City) GetDataByID(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+	result, err := c.cityService.GetDataByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return ctx.Status(fiber.StatusNotFound).JSON(common.APIResponse{
+				Code:    fiber.StatusNotFound,
+				Message: "data not found",
+				Data:    nil,
+			})
+		}
+		return ctx.Status(fiber.StatusInternalServerError).JSON(common.APIResponse{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+			Data:    nil,
+		})
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(common.APIResponse{
+		Code:    fiber.StatusOK,
+		Message: "success",
+		Data:    result,
+	})
+}
+
+func (c *City) Delete(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+	err := c.cityService.Delete(id)
+	if err != nil {
+
+		return ctx.Status(fiber.StatusInternalServerError).JSON(common.APIResponse{
+			Code:    fiber.StatusInternalServerError,
+			Message: err.Error(),
+			Data:    nil,
+		})
+	}
+	return ctx.Status(fiber.StatusOK).JSON(common.APIResponse{
+		Code:    fiber.StatusOK,
+		Message: "success",
+	})
+}
+
 func (c *City) Routes() {
 	group := c.router.Group("/city")
 	group.Get("/", c.GetData)
 	group.Post("/", c.Create)
+	group.Get("/:id", c.GetDataByID)
+	group.Patch("/:id/patch", c.Patch)
+	group.Delete("/:id/delete", c.Delete)
 }
